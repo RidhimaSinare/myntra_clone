4cf2c864a851f94fcc58c71f5a0b7fccc37c8d7a
Ridhima Sinare <ridhisinare@gmail.com>
2023-07-15 17:25:12 +0530

    first commit

---
+++ lib/store_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Store_Page extends StatelessWidget {
+  const Store_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white,);
+  }
+}
