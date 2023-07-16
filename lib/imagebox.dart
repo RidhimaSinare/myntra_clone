99d43a2d6f49b3848564e9b638659263ba9ab5b1
Ridhima Sinare <ridhisinare@gmail.com>
2023-07-16 12:35:00 +0530

    Added page 1 of task

---
+++ lib/imagebox.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImageBox extends StatelessWidget {
+  String imageaddress;
+  String heading;
+  String firstline;
+  String secondline;
+  Color colour;
+
+  ImageBox(
+      {required this.imageaddress,
+      required this.heading,
+      required this.firstline,
+      required this.secondline,
+      required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        height: MediaQuery.of(context).size.height * 0.23,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 27.0, bottom: 10.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  heading,
+                  style: TextStyle(
+                      fontSize: 20,
+                      overflow: TextOverflow.clip,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  firstline,
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
+                Text(
+                  secondline,
+                  style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.transparent,
+                        fixedSize: Size(120, 50),
+                        backgroundColor: Colors.transparent,
+                        padding: EdgeInsets.only(left: 3)),
+                    onPressed: () {},
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Enter Store',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                              color: colour),
+                        ),
+                        Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                          size: 20.0,
+                          semanticLabel:
+                              'Text to announce in accessibility modes',
+                        ),
+                      ],
+                    ))
+              ]),
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                fit: BoxFit.cover, image: NetworkImage(imageaddress))),
+      ),
+    );
+  }
+}
--- lib/store_page.dart
+++ lib/store_page.dart
@@ -1,11 +1,68 @@
 import 'package:flutter/material.dart';
 
+import 'imagebox.dart';
 
 class Store_Page extends StatelessWidget {
   const Store_Page({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 50),
+                child: CircleAvatar(
+                    radius: 40.0,
+                    backgroundImage: AssetImage('images/myntra.jpg')),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30, bottom: 20),
+                child: Text(
+                  'Select Your Store',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w900, color: Color(0xFF3A3B3C)),
+                ),
+              ),
+              ImageBox(
+                imageaddress:
+                    'https://tse2.mm.bing.net/th?id=OIP.220JFeJsCILm1Vc9GARkTwHaDn&pid=Api&P=0&h=180',
+                heading: 'MYNTRA',
+                firstline: "India's Online Fashion &",
+                secondline: 'Beauty Store',
+                colour: Colors.pink,
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              ImageBox(
+                  imageaddress:
+                      'https://tse3.mm.bing.net/th?id=OIP.gZ88J6ZtQDbWqgKTuGkLJwAAAA&pid=Api&P=0&h=180',
+                  heading: 'FWD',
+                  firstline: 'Unmissable Deals On',
+                  secondline: 'Seriously Lit Looks',
+                  colour: Colors.pink),
+              SizedBox(
+                height: 25.0,
+              ),
+              ImageBox(
+                imageaddress:
+                    'https://tse1.mm.bing.net/th?id=OIP.lrcrHGWZH4LEaav9oKnn7QHaEA&pid=Api&P=0&h=180',
+                heading: 'LUXE',
+                firstline: 'The Most Coveted',
+                secondline: 'Luxury Finds',
+                colour: Colors.white,
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
